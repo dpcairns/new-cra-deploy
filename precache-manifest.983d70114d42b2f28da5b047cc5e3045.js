8618a3c3ccdd4f685002eec745860d5c38b512db
Danny Cairns <dpcairns@gmail.com>
2020-02-21 16:33:49 -0800

    Updates

---
+++ precache-manifest.983d70114d42b2f28da5b047cc5e3045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ba23a69401eb4aeff758f020d3f8aa",
+    "url": "/dpcairns/new-cra-deploy/index.html"
+  },
+  {
+    "revision": "88ce3482667bc1c8630e",
+    "url": "/dpcairns/new-cra-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a31a4f156f84111e7190",
+    "url": "/dpcairns/new-cra-deploy/static/js/2.d73c25a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dpcairns/new-cra-deploy/static/js/2.d73c25a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ce3482667bc1c8630e",
+    "url": "/dpcairns/new-cra-deploy/static/js/main.30a2ff19.chunk.js"
+  },
+  {
+    "revision": "7fc6a4a2d63f83d48ef2",
+    "url": "/dpcairns/new-cra-deploy/static/js/runtime-main.932a5ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dpcairns/new-cra-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
